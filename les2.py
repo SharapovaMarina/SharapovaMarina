f84b2ea3f418accacc6996adab0b7bf7a1991f72
SharapovaMarina <sanitzkaja.mar@yandex.ru>
2024-08-14 17:59:26 +0300

    Add files via upload

---
+++ les2.py
@@ -0,0 +1,5 @@
+course_name = 'Python'
+task_count = 12
+hour_count = 1.5
+wasted_for_1_task = hour_count//task_count
+print("Курс:", course_name +",", "всего задач:", task_count, "затрачено часов", hour_count, "среднее время выполнения", hour_count, hour_count/task_count)
